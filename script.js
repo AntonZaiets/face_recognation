04a2419cf9a4968ac115430621b8a51e8482c138
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2025-04-08 17:52:22 +0300

    start tests

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ function loadLabeledImages() {
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
-      for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+      for (let i = 1; i <= 3; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/AntonZaiets/face_recognation/master/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
